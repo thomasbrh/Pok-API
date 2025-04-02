51b1a768c836fb9b674352f8920903cd58c89856
thomasbrh <thomas.bruch@student.heaj.be>
2025-04-02 17:05:09 +0200

    Html + fetch

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,42 +1,48 @@
 'use strict';
-/* Importation de GSAP */
-import gsap from "gsap";
-import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-gsap.registerPlugin(ScrollToPlugin);
-
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Script chargé !");
-    console.log(typeof gsap !== "undefined" ? "GSAP est bien chargé" : "GSAP ne fonctionne pas");
-});
-
 /* Chargement des données JSON */
-let titanicData = [];
-fetch("assets/data/data.json")
-    .then(response => {
+
+var inputText = document.querySelector('.search-input');
+var submit = document.querySelector('.submit');
+var img = document.querySelector('.img');
+
+submit.addEventListener('click', function() {
+    var text = inputText.value;
+    console.log(text);
+    // creer l'url qui correspond a la valeur du champ
+    var url = "https://web.mayfly.ovh/proxy/pokeapi.php?endpoint=pokemon/" + inputText.value;
+    console.log(url);
+    // faire un fetch pour avoir les données json
+
+    fetch(url)
+    .then(function (response) {
         if (!response.ok) {
             throw new Error("Erreur de chargement du fichier JSON");
         }
         return response.json();
     })
-    .then(data => {
-        titanicData = data;
+    .then(function(data) {
+        console.log(data);
+        // afficher le nom à l'écran
+        var name = document.querySelector('.name');
+        name.innerHTML = data.name;
+        // remplir le src du tag img
+        img.setAttribute('src', data.sprites.front_default);
+        // remplir le ul vide avec les types et stats
+        // Selectionne le ul 
+        var listElement = document.querySelector('.list');
+        // Tableau
+        var listing = document.querySelector('.listing');
+        //boucle dans mon tableau
+        listing.forEach(function(item) {
+            // item vaut chaque élément du tableau
+            console.log(item);
+            // créer un li
+            li.innerHTML = item;
+            // ajoute le li dans le ul
+            listElement.appendChild(li);
+        })
     })
     .catch(error => {
         console.error("Erreur lors du chargement des données", error);
     });
-
-/* Barre de progression */
-/* window.onscroll = function() {
-    updateProgressBar();
-};
-
-function updateProgressBar() {
-    const scrollTop = window.scrollY || document.documentElement.scrollTop;
-    const docHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    const progress = (scrollTop / docHeight) * 100;
- 
-    const progressBar = document.getElementById("progress-bar");
-    if (progressBar) {
-        progressBar.style.width = progress + "%";
-    }
-} */
+});
